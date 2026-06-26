011112d75211906f6bf22e008e901d1b7185124b
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-26 15:43:32 +0500

    добавлено открытие камеры

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,21 @@
 // author https://github.com/MIrrox27/ASCII-Camera
 // src/main.cpp
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+
+    // Открываем камеру по умолчанию (в моем случае 1 - встроенная в ноутбук)
+    cv::VideoCapture cap(1);
+    
+    if (!cap.isOpened()) {
+        std::cerr << "-1 Ошибка" << std::endl;
+        return -1;
+    }
+
+    std::cout << "0 Камера запустилась" << std::endl;
+    return 0;
+}
