202831f7e285030dccfb0c733ea89763ef612f32
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-27 21:58:41 +0500

    обновлены функции

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,57 @@
 #include "console.hpp"
 #include <cstdlib>
 #include <string>
+#include <math.h>
+#include <windows.h>
 
 
 int main() {
-    int width = 10;
-    int height = 10;
+    int width = 120;
+    int height = 30;
+    //consoleSize(width, height);
 
-    consoleSize(width, height);
     char gradient[12] = " .,^:;!$#%@";
+    std::cout << 1 << std::endl;  
+    cv::VideoCapture cap;
     
  
     for (int i = 0; i <= 10; i++) {
-        cv::VideoCapture cap(i);
+        cap.open(i);
 
         if (cap.isOpened()) {
         std::cout << "\n\n --- Camera was started! Index: " << i << std::endl;
         break;
         }
     }
-    clearScreen();
-    moveCursor(0, 0);
+
+    if (!cap.isOpened()) {
+        std::cout << "Error: Camera not found!" << std::endl;
+        return -1;
+    }
+
+   // clearScreen();
+   // moveCursor(0, 0);
+    std::cout << 1 << std::endl;  
+
+    cv::Mat frame;
+    cv::Mat gray;
+
     while (true){
+        cap >> frame;
+
+        // Проверяем, что кадр не пустой 
+        if (frame.empty()) {
+            std::cout << "Blank frame grabbed" << std::endl;
+            continue;
+        }
+
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        cv::imshow("Photo", gray);
+        cv::waitKey(1); // нужно для обновления окна
+
+        if (cv::waitKey(1) == 27) {
+            break;
+        }
     }
     
 
