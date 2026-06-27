21be83299551ccbe3ed0a72f547739edb41bf7e8
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-27 22:36:40 +0500

    добавлены настройки камеры

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,41 @@
 
 
 int main() {
+    int width = 120;
+    int height = 30;
+
     char gradient[12] = " .,^:;!$#%@";
+    cv::VideoCapture cap;
  
     for (int i = 0; i <= 10; i++) {
-        cv::VideoCapture cap(i);
-
+        cap.open(i);
         if (cap.isOpened()) {
         std::cout << "\n\n --- Camera was started! Index: " << i << std::endl;
         break;
         }
-    }
+    } 
+
+        // настраиваю кармеры
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, width);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, height);
+    cap.set(cv::CAP_PROP_FPS, 30);
+
+
     clearScreen();
     moveCursor(0, 0);
 
+     cv::Mat frame, gray;
 
+    while (true)
+    {
+        
+    }
+    
+
+
+        // освобождаем ресурсы
+    cap.release();
+    //cv::destroyAllWindows();
+    std::cout << "Programm ended" << std::endl;
     return 0;
 }
